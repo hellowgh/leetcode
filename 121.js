69952b13be6b38fe4eef997259ce3015c998107b
hellowgh <hellowgh@qq.com>
2024-07-15 22:21:18 +0800

    7.12

---
+++ 121.js
@@ -0,0 +1,10 @@
+// best time to buy and sell stock
+
+var maxProfit = function(prices) {
+    let profit = 0;
+    const map = {};
+
+    for (let i = 0; i < prices.length; i++) {
+        
+    }
+};
